bc0e9044e3a5055dcb29ccc81af9bf0ccaa463cc
App Academy Student <student@appacademy.io>
2017-04-12 11:02:12 -0700

    Forge associations amongst 3 models

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,11 @@
 class Course < ActiveRecord::Base
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: "Enrollment"
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
+  
 end
